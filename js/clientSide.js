2a8a0bd41d8b18c3a02bac36c0b2b3afe1f9a13d
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-29 14:02:26 -0500

    initial api groundwork, will move to different branch

--- js/clientSide.js
+++ js/clientSide.js
@@ -1,4 +1,10 @@
 
 // API KEY: 3e435c47205fce9efa0e31699f09a538
 // API CALL URL: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-function getWeather
+async function getWeather() {
+    let city = `Houston`;
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3e435c47205fce9efa0e31699f09a538&units=imperial`)
+    let data = await res.json();
+    console.log(data);
+}
+getWeather();
