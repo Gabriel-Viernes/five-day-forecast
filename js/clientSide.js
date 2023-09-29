4796606dd7636f782bf7d60c2a83e52e3bb92017
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-29 14:26:25 -0500

    added function that displays usage instructions when the user enters something into the search box

--- js/clientSide.js
+++ js/clientSide.js
@@ -1,10 +1,24 @@
 
 // API KEY: 3e435c47205fce9efa0e31699f09a538
 // API CALL URL: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+let searchContain = document.getElementById('searchContain')
+let inputEl = document.getElementById(`locationInput`);
 async function getWeather() {
-    let city = `Houston`;
+    let city = document.getElementById(`locationInput`).value;
     const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3e435c47205fce9efa0e31699f09a538&units=imperial`)
     let data = await res.json();
     console.log(data);
 }
-getWeather();
+
+function searchInstruct(e) {
+    console.log(`searchinstruct added`)
+    console.log(e.code);
+    if (searchContain.lastChild.textContent === "Press enter to search") {
+    } else {
+        let userInstruct = document.createElement('p');
+        userInstruct.textContent = "Press enter to search"
+        userInstruct.setAttribute('style', 'font-family: "Traveling-Typewriter"; font-size: 2em')
+        searchContain.append(userInstruct);
+    }    
+}
+inputEl.addEventListener('keydown', searchInstruct);
